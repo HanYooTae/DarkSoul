74d271819038c3237637ff86a2c784fd632136b5
Yoo Tae Han <han931120@gmail.com>
2026-04-01 20:03:47 +0900

    GameMode, Character, PlayerController 생성

--- Source/DarkSoul/DarkSoul.Build.cs
+++ Source/DarkSoul/DarkSoul.Build.cs
@@ -7,8 +7,11 @@ public class DarkSoul : ModuleRules
 	public DarkSoul(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PublicIncludePaths.AddRange(
+			new string[] { "DarkSoul" });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
